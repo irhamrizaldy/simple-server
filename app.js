f45a849a16fb49b40849c1c24d348fa8262c56b3
irhamrizaldy <43570194+irhamrizaldy@users.noreply.github.com>
2022-03-18 03:42:55 +0700

    init commit

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ const server = http.createServer((req, res) => {
     res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
     console.log(`Server running at http://${port}/`);
 });
