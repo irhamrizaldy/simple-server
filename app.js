1baa15b2193b951c059ada8542b8ba6f8438f261
irhamrizaldy <43570194+irhamrizaldy@users.noreply.github.com>
2022-03-18 03:39:42 +0700

    init commit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-// PORT
-const PORT = process.env.PORT || 8081;
+const app = require('http').createServer((req, res) => res.send('Ahoy!'));
+const PORT = process.env.PORT || 3000;
 
-// Listen on port 8081
-app.listen(PORT, () =>
-    console.log(`Application is listening on port ${PORT}!`)
-);
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
